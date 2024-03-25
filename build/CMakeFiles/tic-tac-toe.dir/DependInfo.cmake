3857d6faa8faba8dc228d0c13a05176deb7d7d4c
L4F7 <l4f7@DESKTOP-I5NQNC1.>
2024-03-24 18:19:32 -0600

    Cmake build added

---
+++ build/CMakeFiles/tic-tac-toe.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/l4f7/paralelas/tic-tac-toe/src/board.cpp" "CMakeFiles/tic-tac-toe.dir/src/board.cpp.o" "gcc" "CMakeFiles/tic-tac-toe.dir/src/board.cpp.o.d"
+  "/home/l4f7/paralelas/tic-tac-toe/src/bot.cpp" "CMakeFiles/tic-tac-toe.dir/src/bot.cpp.o" "gcc" "CMakeFiles/tic-tac-toe.dir/src/bot.cpp.o.d"
+  "/home/l4f7/paralelas/tic-tac-toe/src/main.cpp" "CMakeFiles/tic-tac-toe.dir/src/main.cpp.o" "gcc" "CMakeFiles/tic-tac-toe.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/tic-tac-toe.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tic-tac-toe.dir/src/board.cpp.o"
+  "CMakeFiles/tic-tac-toe.dir/src/board.cpp.o.d"
+  "CMakeFiles/tic-tac-toe.dir/src/bot.cpp.o"
+  "CMakeFiles/tic-tac-toe.dir/src/bot.cpp.o.d"
+  "CMakeFiles/tic-tac-toe.dir/src/main.cpp.o"
+  "CMakeFiles/tic-tac-toe.dir/src/main.cpp.o.d"
+  "tic-tac-toe"
+  "tic-tac-toe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tic-tac-toe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
