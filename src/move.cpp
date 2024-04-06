ca374a7b9ec990bec3017b0d95658cc58163d593
L4F7 <fuentes4f7@gmail.com>
2024-04-06 12:50:53 -0600

    Add move.cpp

---
+++ src/move.cpp
@@ -0,0 +1,29 @@
+#include "move.h"
+
+Move::Move() {
+    this->row = 0;
+    this->col = 0;
+}
+
+Move::Move(int row, int col) {
+    this->row = row;
+    this->col = col;
+}
+
+Move::~Move() {}
+
+int Move::getRow() const {
+    return row;
+}
+
+int Move::getCol() const {
+    return col;
+}
+
+void Move::setRow(int row) {
+    this->row = row;
+}
+
+void Move::setCol(int col) {
+    this->col = col;
+}
